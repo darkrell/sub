e1fc373608bc2606080a128a63f9cc7d849dec3c
darkrell <rc-alh@outlook.com>
2023-04-29 16:10:26 +0400

    Empty maui blazor project created

---
+++ Sub/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Sub;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ Sub/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Sub;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
